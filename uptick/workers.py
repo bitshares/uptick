ad929893719b85f031ad1cbee5a9a75a012e5d38
Fabian Schuh <Fabian@chainsquad.com>
2017-11-10 11:37:35 +0100

    Allow to list all workers

--- uptick/workers.py
+++ uptick/workers.py
@@ -1,5 +1,11 @@
+import json
 import click
+import datetime
+from prettytable import PrettyTable
 from bitshares.storage import configStorage as config
+from bitshares.worker import Workers
+from bitshares.account import Account
+from bitshares.amount import Amount
 from pprint import pprint
 from .decorators import (
     onlineChain,
@@ -48,3 +54,45 @@ def disapproveworker(ctx, workers, account):
         workers,
         account=account
     ))
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    'account',
+    default=None,
+    required=False
+)
+def workers(ctx, account):
+    """ List all workers (of an account)
+    """
+    workers = Workers(account)
+    t = PrettyTable([
+        "id",
+        "name/url",
+        "daily_pay",
+        "votes",
+        "time",
+        "account",
+    ])
+    t.align["name/url"] = "l"
+    t.align["account"] = "l"
+    for worker in workers:
+        if worker["work_end_date"] < datetime.datetime.utcnow():
+            continue
+        votes = Amount({
+            "amount": worker["total_votes_for"],
+            "asset_id": "1.3.0"})
+        amount = Amount({
+            "amount": worker["daily_pay"],
+            "asset_id": "1.3.0"})
+        t.add_row([
+            worker["id"],
+            "{name}\n{url}".format(**worker),
+            str(amount),
+            str(votes),
+            "{work_begin_date:%Y-%m-%d}\n-\n{work_end_date:%Y-%m-%d}".format(**worker),
+            str(Account(worker["worker_account"])["name"]),
+        ])
+    click.echo(t)
